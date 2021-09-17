c0b39ebfbf1eba80ab3e199eb925c983dae769d8
mspicola <mspicola@protonmail.com>
2021-09-17 02:54:19 -0400

    finished all taskes but still failing some tests, unsure as to issue

--- index.js
+++ index.js
@@ -173,13 +173,13 @@ class Student extends Lambdasian {
      this.favSubjects = favSubjects;
    }
    listSubjects(){
-
+    return `Loving ${this.favSubjects}`;
    }
-   PRAssignment(){
-
+   PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
    }
-   sprintChallenge(){
-     
+   sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
    }
 }
 
@@ -196,8 +196,18 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }   
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${student}`;
+  }
 }
 /*
   STRETCH PROBLEM (no tests!)
